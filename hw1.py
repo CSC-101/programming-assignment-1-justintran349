6c290a46c6c12f43a5d6d17ea30f25dddc3ee4e7
bieutran <jtran349@calpoly.edu>
2024-10-23 17:56:42 -0700

    1st Task!

--- hw1.py
+++ hw1.py
@@ -7,8 +7,9 @@ def vowel_count(word: str)-> int:
     vowels = "AaEeIiOoUu"
     count = sum(1 for vowel in word if vowel in vowels)
     return count
+#counts the vowels in the unputted word
 # Part 2
-
+def short_lists(list1: list[list[int]])
 
 # Part 3
 
